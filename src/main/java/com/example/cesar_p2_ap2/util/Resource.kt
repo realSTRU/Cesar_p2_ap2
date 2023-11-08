2b080d4fd08a4df39bac09407cb628cf0e854ad9
Cesar Reynoso <cesarreynoso0969@gmail.com>
2023-11-08 18:34:51 -0400

    First commit to init my exam attempt

---
+++ src/main/java/com/example/cesar_p2_ap2/util/Resource.kt
@@ -0,0 +1,7 @@
+package com.example.cesar_p2_ap2.util
+//sealed class to manage the API gastos.
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T?): Resource<T>(data)
+    class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
+    class Loading<T>(val isLoading: Boolean = true): Resource<T>(null)
+}
