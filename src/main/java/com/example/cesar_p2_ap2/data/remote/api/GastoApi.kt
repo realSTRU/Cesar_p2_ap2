bc0a944e31c4c2526158dbf69632a2687010a258
Cesar Reynoso <cesarreynoso0969@gmail.com>
2023-11-08 19:13:20 -0400

    doing dto and api

---
+++ src/main/java/com/example/cesar_p2_ap2/data/remote/api/GastoApi.kt
@@ -0,0 +1,28 @@
+package com.example.cesar_p2_ap2.data.remote.api
+
+import com.example.cesar_p2_ap2.data.remote.dto.GastoDto
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface GastoApi {
+    @GET("api/Gastos")
+    suspend fun getGastos(): List<GastoDto>
+    @GET("api/Gastos/{id}")
+    suspend fun getGastoById(@Path("idGasto") idGasto: Int) : GastoDto
+    @POST("api/Gastos")
+    suspend fun postGasto(@Body gato : GastoDto) : GastoDto
+    @PUT("api/Gastos/{id}")
+    suspend fun postGasto(@Body gato : GastoDto, @Path("idGasto") idGasto: Int) : GastoDto
+    @DELETE("api/Gastos/{id}")
+    suspend fun deleteGasto(@Path("idGasto") idGasto: Int) : GastoDto
+
+
+
+
+
+
+}
---
+++ src/main/java/com/example/cesar_p2_ap2/data/remote/dto/GastoDto.kt
@@ -0,0 +1,24 @@
+package com.example.cesar_p2_ap2.data.remote.dto
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+//Dto de gastos modelado para hacer la ui propuesta para el examen.
+@JsonClass(generateAdapter = true)
+data class GastoDto (
+
+    @Json(name = "idGasto")
+    val idGasto : Int?,
+    @Json(name = "fecha")
+    val fecha : String,
+    @Json(name = "suplidor")
+    val suplidor : String,
+    @Json(name = "concepto")
+    val concepto : String,
+    @Json(name="ncf")
+    val ncf : String,
+    @Json(name="itbis")
+    val itbis : Double,
+    @Json(name = "monto")
+    val monto : Double
+)
