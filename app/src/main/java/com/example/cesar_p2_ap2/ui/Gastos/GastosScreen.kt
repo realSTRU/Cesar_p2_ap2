ab5ca107edf088f1377c37b4f87905f2dbbd9c74
Cesar Reynoso <cesarreynoso0969@gmail.com>
2023-11-10 14:30:58 -0400

    Consult finished now fixing register

--- app/src/main/java/com/example/cesar_p2_ap2/ui/Gastos/GastosScreen.kt
+++ app/src/main/java/com/example/cesar_p2_ap2/ui/Gastos/GastosScreen.kt
@@ -6,16 +6,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DeleteOutline
+import androidx.compose.material.icons.filled.SaveAs
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -56,7 +66,6 @@ fun GastosScreen (
                         .padding(top = 70.dp)
                 ) {
                     items(gastos) { gastos ->
-                        Divider()
                         RowItemForAnGasto(gasto = gastos)
                     }
                 }
@@ -103,56 +112,103 @@ fun Register()
 @Composable
 fun RowItemForAnGasto(gasto : GastoDto)
 {
-    Surface(
+    ElevatedCard(
+
         modifier = Modifier
-            .padding(10.dp)
-            .fillMaxWidth(),
-        color = MaterialTheme.colorScheme.onTertiary,
-        shadowElevation = 5.dp,
-        shape = RoundedCornerShape(8.dp)
+            .fillMaxWidth()
+            .padding(8.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.elevatedCardElevation()
+
     ) {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(8.dp)
+        )
+        {
+            val parseDate = LocalDateTime.parse(gasto.fecha, DateTimeFormatter.ISO_DATE_TIME)
+            val formatedDate = parseDate.format(DateTimeFormatter.ISO_DATE)
             Row(
-                horizontalArrangement = Arrangement.Start
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
             )
             {
-                Text(text="ID: "+gasto.idGasto.toString(),)
-                Spacer(modifier = Modifier.padding(end=80.dp))
-                Text(text="fecha:"+ formatearFecha(gasto.fecha))
+                Text(text="ID:${gasto.idGasto}"
+                    ,style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.weight(3f)
+                )
+                Spacer(modifier = Modifier.width(46.dp))
+                Text(text="Fecha: $formatedDate",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.weight(4f),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
             }
-            Row {
-                Text(text = "Claro Dominiciana", style = MaterialTheme.typography.titleMedium, color = MaterialTheme.colorScheme.primary)
-            }
-            Row(modifier = Modifier.padding(start=15.dp)){
-                Text(text ="Concepto:"+gasto.concepto, maxLines = 2, overflow = TextOverflow.Ellipsis)
-            }
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Column{
-                    Text(text = "ncf:" + gasto.ncf)
-                    Text(text = "Itbis:" + gasto.itbis)
+            Spacer(modifier = Modifier.height(6.dp))
 
+                    Text(
+                        "${gasto.suplidor}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+
+
+                    Text(
+                        "${gasto.concepto}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis
+                    )
+
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(2.dp),
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text("NCF: ${gasto.ncf}", style = MaterialTheme.typography.bodyMedium)
+                    Text(
+                        "ITBIS: ${gasto.itbis}", style = MaterialTheme.typography.bodyMedium
+                    )
                 }
-                Column() {
-                    Text(text = "Monto:" + gasto.monto, color = MaterialTheme.colorScheme.error)
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(
+                    "$${gasto.monto}",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
+            Spacer(modifier = Modifier.height(6.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp)
+            ) {
+                ElevatedButton(
+                    onClick = { },
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(end = 4.dp)
+                ) {
+                    Text("Modificar")
+                }
+                OutlinedButton(
+                    onClick = { },
+                    modifier = Modifier
+                        .weight(1.1f)
+                        .padding(start = 4.dp),
+                    //colors = ButtonDefaults.buttonColors(Color.Red)
+                ) {
+                    Icon(imageVector = Icons.Default.DeleteOutline, contentDescription = "Delete")
+                    Text("Eliminar")
                 }
             }
         }
 
     }
-}
-fun formatearFecha(fechaOriginal: String): String {
-    val formatoOriginal = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
-    val fechaParseada = LocalDateTime.parse(fechaOriginal, formatoOriginal)
-    val formatoDeseado = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-    return fechaParseada.format(formatoDeseado)
-}
-
-fun main() {
-    val fechaOriginal = "2023-11-08T23:40:08.747"
-    val fechaFormateada = formatearFecha(fechaOriginal)
-    println(fechaFormateada) // Imprime: 2023-11-08
 }
